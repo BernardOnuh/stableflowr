b7003e9487fec067d4a0253308e438f91f7abdb3
Bernard.O <114490070+BernardOnuh@users.noreply.github.com>
2026-04-18 13:40:17 +0100

    @  updated

--- app/page.tsx
+++ app/page.tsx
@@ -4,21 +4,19 @@ import Hero from "@/components/Hero";
 import Features from "@/components/Features";
 import SdkApi from "@/components/SdkApi";
 import CTA from "@/components/CTA";
-import Footer from "@/components/Footer";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
     <>
       <Cursor />
       <Navbar />
-
       <main>
         <Hero />
         <Features />
         <SdkApi />
         <CTA />
       </main>
-      
       <Footer />
     </>
   );
