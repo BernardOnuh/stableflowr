db86a4d97778f6b1ffe2228d0abfe8f912ab4700
BernardOnuh <onuhbernard4@gmail.com>
2026-04-25 12:06:28 +0100

    sdk: stacks->base USDC 1000

--- components/SdkApi.tsx
+++ components/SdkApi.tsx
@@ -23,9 +23,9 @@ const API_CODE = `curl -X POST https://api.stableflowr.xyz/v1/transfer \\
   -H "Authorization: Bearer YOUR_KEY" \\
   -H "Content-Type: application/json" \\
   -d '{
-    "from":      { "chain": "stacks", "token": "USDC" },
+    "from":      { "chain": "arbitrum", "token": "USDC" },
     "to":        { "chain": "arbitrum", "token": "USDC" },
-    "amount":    "2000",
+    "amount":    "500",
     "recipient": "0xabcd...def"
   }'
 
@@ -34,7 +34,7 @@ const API_CODE = `curl -X POST https://api.stableflowr.xyz/v1/transfer \\
   "txHash":  "0x3f9a...",
   "status":  "settled",
   "feePaid": "0.00",
-  "timeMs":  1769
+  "timeMs":  2225
 }`;
 
 function CodeHeader({ label, sub }: { label: string; sub: string }) {
